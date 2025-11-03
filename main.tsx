79cfbb96d2c65be0f2ab526763f9243f4baa60c1
Delinqs <emmanueldeveloper247@gmail.com>
2025-11-03 10:44:45 +0100

    Add files via upload

---
+++ main.tsx
@@ -0,0 +1,15 @@
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom/client';
+import App from './App';
+
+import './index.css';
+
+// Use light mode by default for the purple theme
+document.documentElement.classList.remove('dark');
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
